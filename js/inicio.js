52563cc7b9adae61a5bbc3736439a130365274fc
wladoytsolutions <wmoya@oytsolutions.cl>
2016-08-18 16:02:40 -0400

    ..

--- js/inicio.js
+++ js/inicio.js
@@ -18,6 +18,50 @@ Highcharts.setOptions({
     colors: Colores
 });
 
+
+            document.addEventListener('deviceready', function(event) 
+            {
+            	var leanModeButton = document.getElementById('leanModeButton'),
+					immersiveModeButton = document.getElementById('immersiveModeButton'),
+            		showUnderUiButton = document.getElementById('showUnderUiButton'),
+            		showUiButton = document.getElementById('showUiButton');
+            	
+            	function successFunction()
+            	{
+            	    console.log("It worked!");
+            	}
+            	function errorFunction(error)
+            	{
+            	    console.log(error);
+            	}
+				
+            	function trace(value)
+            	{
+            	    console.log(value);
+            	}
+            	
+            	leanModeButton.addEventListener('click', function(event)
+            	{
+            		AndroidFullScreen.leanMode(successFunction, errorFunction);
+            	});
+            	
+            	immersiveModeButton.addEventListener('click', function(event)
+            	{
+            		AndroidFullScreen.immersiveMode(successFunction, errorFunction);
+            	});
+            	
+            	showUnderUiButton.addEventListener('click', function(event)
+             	{
+             		AndroidFullScreen.showUnderSystemUI(successFunction, errorFunction);
+             	});
+            	
+            	showUiButton.addEventListener('click', function(event)
+             	{
+             		AndroidFullScreen.showSystemUI(successFunction, errorFunction);
+             	});
+            	
+            });
+
 $( document ).ready(function() {	
 	setInterval(function(){ ParpadearAlarmaLocal(); }, 1000);
 	
