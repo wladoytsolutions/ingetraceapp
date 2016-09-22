545133f2dd7723f2adf8be292c0560c327d53602
wladoytsolutions <wmoya@oytsolutions.cl>
2016-09-22 18:09:00 -0400

    .

--- www/js/index.js
+++ www/js/index.js
@@ -23,7 +23,7 @@ var app = {
 		var push = PushNotification.init({
 			android: {
 				senderID: "964841478681",
-				sound: "raw/nokia.mp3", 
+				sound: "nokia.mp3", 
                 forceShow: true,
                 vibrate: true	
 			},
