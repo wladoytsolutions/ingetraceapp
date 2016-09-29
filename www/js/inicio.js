5e3438da210e3e1e4d99764081ee89341c92e0b2
wladoytsolutions <wmoya@oytsolutions.cl>
2016-09-29 11:53:40 -0400

    .

--- www/js/inicio.js
+++ www/js/inicio.js
@@ -64,6 +64,7 @@ function ScrollContenedor(Id_sensor)
 function CargarGraficoSensorTermico(event,IdCliente,NombreCliente,IdSucursal,NombreSucursal,IdSeccion,NombreSeccion,IdEquipo,NombreEquipo,IdSensor)
 {
 	event.preventDefault();
+	alert(''+getActivePage());
 	VerGraficoSensorTermico('ModalPage2',false,IdCliente,NombreCliente,IdSucursal,NombreSucursal,IdSeccion,NombreSeccion,IdEquipo,NombreEquipo,IdSensor);
 }
 function CargarGraficoSensorElectrico(event,IdSensor,NombreEquipo)
