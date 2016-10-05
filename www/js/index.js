d29cabb9e0f8fd54fe8a788c00dcb85efeed2252
wladoytsolutions <wmoya@oytsolutions.cl>
2016-10-05 18:31:54 -0400

    ..

--- www/js/index.js
+++ www/js/index.js
@@ -840,7 +840,7 @@ function login()
 			{				
 				//Cookie
 				setCK(''+d.CK);
-				setJsonSucursal(d.ID_CLIENTE,d.ID_SUC,response);
+				//setJsonSucursal(d.ID_CLIENTE,d.ID_SUC,response);
 				setIdDevice($("#H_TEXT_DEVICE").html());
 				
 				//Cargando html
