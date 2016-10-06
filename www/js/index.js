5b286a80436acb7dc4ddb958712a6384a341f895
wladoytsolutions <wmoya@oytsolutions.cl>
2016-10-06 14:40:15 -0400

    ..

--- www/js/index.js
+++ www/js/index.js
@@ -158,15 +158,11 @@ function CerrarSplash()
 }
 function setJsonSucursal(id_cliente,id_sucursal,json)
 {
-	alert("Ingresando datos de BD json_>"+json);
 	var StringJson=""+Base64.encode(""+json);
 	
-	alert(StringJson);
-	BD_APP = window.sqlitePlugin.openDatabase({name: "ingetrace.db", location: 'default', createFromLocation: 1});
 	BD_APP.transaction(function(tx) {
 		var StringQuery="UPDATE tbl_datos SET id_cliente='"+id_cliente+"', id_sucursal='"+id_sucursal+"', json_sucursal='"+StringJson+"'";		
 		tx.executeSql(StringQuery);
-		alert("Ingresado a BD");
 	});
 }
 function setIdDevice(IdDevice)
