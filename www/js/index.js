47ab56589d55724b96971d19b6f3648d2ed148b1
wladoytsolutions <wmoya@oytsolutions.cl>
2016-10-06 14:48:17 -0400

    .

--- www/js/index.js
+++ www/js/index.js
@@ -160,6 +160,7 @@ function setJsonSucursal(id_cliente,id_sucursal,json)
 {
 	var StringJson=""+Base64.encode(""+json);
 	
+	BD_APP = window.sqlitePlugin.openDatabase({name: "ingetrace.db", location: 'default', createFromLocation: 1});
 	BD_APP.transaction(function(tx) {
 		var StringQuery="UPDATE tbl_datos SET id_cliente='"+id_cliente+"', id_sucursal='"+id_sucursal+"', json_sucursal='"+StringJson+"'";		
 		tx.executeSql(StringQuery);
