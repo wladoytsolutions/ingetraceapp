c6e5994879e0dfbd3fd3c9e33cd3d7ad8f0da0c4
wladoytsolutions <wmoya@oytsolutions.cl>
2016-10-21 15:23:22 -0400

    ..

--- www/js/index.js
+++ www/js/index.js
@@ -39,7 +39,7 @@ var app = {
 
 		pushPlugin = PushNotification.init({
 			android: {
-				senderID: "485509697255",
+				senderID: "570571190177",
 				sound: true, 
                 forceShow: true,
                 vibrate: true	
