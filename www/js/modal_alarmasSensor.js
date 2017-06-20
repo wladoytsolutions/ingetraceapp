c40cf040db65ba52992aee92f0ab4ccae2b783d6
wladoytsolutions <wmoya@oytsolutions.cl>
2017-06-20 15:38:28 -0400

    .

--- www/js/modal_alarmasSensor.js
+++ www/js/modal_alarmasSensor.js
@@ -71,7 +71,7 @@ function GuardarBitacora()
 				accion 		: 'GuardarBitacora',
 				ID_ALERTA   : $('#H_ID_ALERTA').val(),
 				COMENTARIO  : $('#txtcomentario_bitacora').val(),
-				CK			: getCK()
+				CK			: ''+getCookie('INGSCE_INF')
 				}, 
 	function(response) {			
 		var json = jQuery.parseJSON(response);
@@ -122,4 +122,57 @@ function RecargarTabla()
 	
 	var tablaProblemas2=$(divProblemas2).find('table');
 	$(tablaProblemas2).css('width','100%');
+}
+function ValidarFechasOperaciones()
+{
+	var Valido=false;
+	var Respuesta="";
+	
+	var startDate = $('#inicio_filtroDatosSensor').val();
+	startDate=String(startDate);
+		
+	//Convertiendo a float
+	var anioIni=startDate.substring(6, 10);
+	var mesIni=startDate.substring(3, 5);
+	var diaIni=startDate.substring(0, 2);
+		
+	var FecIni = parseFloat(anioIni+mesIni+diaIni);
+		
+	var endDate = $('#termino_filtroDatosSensor').val();
+	endDate=String(endDate);
+		
+	//Convertiendo a float
+	var anioFin=endDate.substring(6, 10);
+	var mesFin=endDate.substring(3, 5);
+	var diaFin=endDate.substring(0, 2);
+	
+	var FecFin = parseFloat(anioFin+mesFin+diaFin);
+		
+	if(FecIni!=FecFin)
+	{
+		if(FecIni < FecFin){
+		   Valido=true;
+		   Respuesta="ok";
+		}
+		else
+		{
+			Respuesta="intervalo";
+		}
+	}
+	else
+	{
+		Valido=true;
+		Respuesta="ok";
+	}
+	if(Valido)
+	{
+		var Dif=FecFin-FecIni;
+		if(Dif>7)
+		{
+			Valido=false;
+			Respuesta="exceso";
+		}
+	}
+		
+	return Respuesta;
 }
