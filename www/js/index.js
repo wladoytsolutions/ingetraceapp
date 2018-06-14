0b0af42289dd8c1d112b4b8d198c6415d7f65876
wladoytsolutions <wmoya@oytsolutions.cl>
2018-06-14 13:21:18 -0400

    .

--- www/js/index.js
+++ www/js/index.js
@@ -102,6 +102,8 @@ var app = {
 					}
 					else
 					{
+						//Si es nada se registrara en BD local
+						setIdDevice(ID_device);
 						setTimeout(function () {
 							if($('#H_DESDE_NOTIFICACION').val()!='1')
 							{
@@ -115,8 +117,6 @@ var app = {
 		});
 
 		pushPlugin.on('notification', function(data) {
-			
-			alert('ACA');
 			$('#H_DESDE_NOTIFICACION').val('1');
 
 			var ID_CLIENTE;
@@ -132,8 +132,6 @@ var app = {
 
 			TITULO_NOTIFICACION=''+data.title;
 			MENSAJE_NOTIFICACION=''+data.message;
-			
-			alert(TITULO_NOTIFICACION);
 
 			ID_CLIENTE=data.additionalData.info.id_cliente;
 			NOMBRE_CLIENTE=data.additionalData.info.nombre_cliente;
